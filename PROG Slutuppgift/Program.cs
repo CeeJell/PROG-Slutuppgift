866f0dd05b30a441d2fe3dcc35e47025b6912033
Charlie Larsson <charlie.larsson@realgymnasiet.se>
2022-04-26 15:46:17 +0200

    Add project files.

---
+++ PROG Slutuppgift/Program.cs
@@ -0,0 +1,69 @@
+﻿using PROG_Slutuppgift;
+
+class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to a Single User Dungeon, made by Charlie");
+
+        Room room1 = new Room { Description = "" };
+        Room room2 = new Room { Description = "" };
+        Room room3 = new Room { Description = "" };
+
+
+        Room currentRoom = room1;
+        
+
+
+        // Gameloop
+        while (true)
+        {
+            Console.WriteLine("Where do you want to go? (N)orth, (S)outh, (E)ast or (W)est");
+            var keyPressed = Console.ReadKey(true);
+
+            Room tmpRoom = null;    
+            switch (keyPressed.Key)
+            {
+                case ConsoleKey.N:
+                    tmpRoom = currentRoom.GoNorth;
+                    break;
+                case ConsoleKey.S:
+                    tmpRoom = currentRoom.GoSouth;
+                    break;
+                case ConsoleKey.E:
+                    tmpRoom = currentRoom.GoEast;
+                    break;
+                case ConsoleKey.W:
+                    tmpRoom = currentRoom.GoWest;
+                    break;
+                default:
+                    System.Console.Write("Not one of the options, ");
+                    break;
+            }
+
+            if (tmpRoom == null)
+            {
+                System.Console.WriteLine("You can't go that way");
+                System.Console.WriteLine();
+                continue;
+            }
+            currentRoom = tmpRoom;
+            Console.WriteLine(currentRoom.Description);
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ PROG Slutuppgift/Room.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG_Slutuppgift
+{
+    public class Room
+    {
+        private Room _north;
+        private Room _south;
+        private Room _east;
+        private Room _west;
+
+        public string Description { get; set; } = "";
+
+        public Room GoSouth => _south;
+        public Room GoEast => _east;
+        public Room GoWest => _west;
+        public Room GoNorth => _north; 
+
+
+        public void AddToNorth(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom._south = this;
+            _north = otherRoom;
+        }
+        public void AddToSouth(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom._north = this;
+            _south = this;
+        }
+
+        public void AddToEast(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom._west = this;
+            _east = otherRoom;
+        }
+
+        public void AddToWest(Room otherRoom, bool oneway = false)
+        {
+            if (!oneway)
+                otherRoom._east = this;
+            _west = otherRoom;
+        }
+
+
+    }
+}
